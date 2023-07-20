ca6d2a62f69aaf6c62dd10ab9609a7b1181bfd4f
kkazydub <kkazydub@gmail.com>
2023-07-20 14:05:57 +0300

    Update responses

--- Commands/EnvCommand.php
+++ Commands/EnvCommand.php
@@ -18,7 +18,7 @@ class EnvCommand extends Command
         $this->setDescription('Add required vars');
     }
 
-    public function __construct(private LoggerInterface $logger)
+    public function __construct(private LoggerInterface $logger, private string $root)
     {
         parent::__construct();
     }
@@ -26,19 +26,20 @@ class EnvCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $path = __DIR__.'/../../../../.env.local';
-        
-        if (!file_exists($path)){
+        $path = $this->root . '/.env';
+
+        if (!file_exists($path)) {
             touch($path);
         }
-        
+
         try {
             $newEnvs = "PAYTECH_API_URL=http://processing.local/" . PHP_EOL .
-                "PAYTECH_MERCHANT_KEY=1234567890"  . PHP_EOL .
+                "PAYTECH_MERCHANT_KEY=1234567890" . PHP_EOL .
                 "PAYTECH_MERCHANT_NAME=TestMerchant";
 
             file_put_contents($path, $newEnvs, FILE_APPEND | LOCK_EX);
-                return Command::SUCCESS;
+            $io->success('Env vars added');
+            return Command::SUCCESS;
         } catch (\Throwable $exception) {
             $this->logger->error($exception->getMessage());
             return Command::FAILURE;
